ee152aedb04230d9bbce36cd7b0a5b40d667e327
aqifcse <aqif.cse@gmail.com>
2022-06-07 16:56:54 +0600

    API url changed

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://morning-sands-52405.herokuapp.com/rental/movies/",
+    baseURL: "https://morning-sands-52405.herokuapp.com/rental/rents/",
     headers: {
         'Accept':'application/json',
         'Content-Type':'application/json',
